3bee25a70be368199b2676732d882e7ca6e1776f
Roman <krr.hjvfy@gmail.com>
2025-02-11 18:05:05 +0700

    More complex example

--- SharedTestIssue/Counter.swift
+++ SharedTestIssue/Counter.swift
@@ -5,36 +5,36 @@ import SwiftUI
 struct Counter {
     @ObservableState
     struct State: Equatable {
-        @Shared(value: 0) var count
-        var label = "Start"
+        var rows: IdentifiedArrayOf<Row.State> = []
+        var started = false
+        var currentId: UUID? = nil
     }
     
     enum Action {
-        case startButtonTapped
-        case increment
-        case stop
+        case buttonTapped
+        case rows(IdentifiedActionOf<Row>)
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .startButtonTapped:
-                state.label = "Counting..."
-                return .run { send in
-                    @Dependency(\.continuousClock) var clock
-                    for _ in 1...3 {
-                        try await clock.sleep(for: .seconds(1))
-                        await send(.increment)
-                    }
-                    await send(.stop)
-                }
-            case .increment:
-                state.$count.withLock { $0 += 1 }
-                return .none
-            case .stop:
-                state.label = "✅"
-                return .none
+            case .buttonTapped:
+                state.rows = .init(uniqueElements: [.init(), .init(), .init()])
+                state.started = true
+                guard let firstId = state.rows.first?.id else { return .none }
+                state.currentId = firstId
+                return .send(.rows(.element(id: firstId, action: .start)))
+            case let .rows(.element(id: id, action: action)):
+                guard case .finish = action,
+                      let currentIndex = state.rows.index(id: id),
+                      currentIndex < state.rows.index(before: state.rows.endIndex) else { return .none }
+                let nextId = state.rows[state.rows.index(after: currentIndex)].id
+                state.currentId = nextId
+                return .send(.rows(.element(id: nextId, action: .start)))
             }
+        }
+        .forEach(\.rows, action: \.rows) {
+            Row()
         }
     }
 }
@@ -43,18 +43,15 @@ struct CounterView: View {
     let store: StoreOf<Counter>
     
     var body: some View {
-        VStack {
-            Text("Count: \(store.count)")
-                .font(.title)
-                .padding(.top, 10)
-            
-            Button(store.label) {
-                store.send(.startButtonTapped)
-            }
-            .buttonStyle(.borderedProminent)
-            .padding(.top, 10)
+        if !store.started {
+            Button("Start") { store.send(.buttonTapped) }
         }
-        .padding()
+        List {
+            ForEach(store.scope(state: \.rows, action: \.rows)) { row in
+                RowView(store: row)
+            }
+        }
+        .listStyle(PlainListStyle())
     }
 }
 
