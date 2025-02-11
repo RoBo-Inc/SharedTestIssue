e7af21e7d149bf3a37290dba8a81fe8a39046cf3
Roman <krr.hjvfy@gmail.com>
2025-02-11 16:17:55 +0700

    Fix Tests

--- SharedTestIssueTests/CounterTests.swift
+++ SharedTestIssueTests/CounterTests.swift
@@ -7,23 +7,21 @@ import Testing
 struct SharedTestIssueTests {
     @Test
     func startButtonTapped() async {
+        let clock = TestClock()
         let store = TestStore(initialState: .init()) {
             Counter()
         } withDependencies: {
-            $0.continuousClock = ImmediateClock()
+            $0.continuousClock = clock
         }
         await store.send(.startButtonTapped) {
             $0.label = "Counting..."
         }
-        await store.receive(\.increment) {
-            $0.$count.withLock { $0 = 1 }
-        }
-        await store.receive(\.increment) {
-            $0.$count.withLock { $0 = 2 }
-        }
+        await clock.run()
         await store.receive(\.increment) {
             $0.$count.withLock { $0 = 3 }
         }
+        await store.receive(\.increment)
+        await store.receive(\.increment)
         await store.receive(\.stop) {
             $0.label = "✅"
         }
