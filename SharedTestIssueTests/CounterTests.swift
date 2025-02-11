614ab1749956318160c4f09b9fe711ee3b90257c
Roman <krr.hjvfy@gmail.com>
2025-02-11 21:35:31 +0700

    @Test

--- SharedTestIssueTests/CounterTests.swift
+++ SharedTestIssueTests/CounterTests.swift
@@ -1,5 +1,4 @@
 import ComposableArchitecture
-import Foundation
 import Testing
 @testable import SharedTestIssue
 
@@ -7,30 +6,48 @@ import Testing
 struct SharedTestIssueTests {
     @Test
     func startButtonTapped() async {
-        let clock = TestClock()
         let rows: IdentifiedArrayOf<Row.State> = withDependencies {
             $0.uuid = .incrementing
-        } operation: { .init(uniqueElements: [.init(), .init(), .init()]) }
+        } operation: {
+            .init(uniqueElements: [0, 0, 0].map(Row.State.init))
+        }
+        let clock = TestClock()
         let store = TestStore(initialState: .init()) {
             Counter()
         } withDependencies: {
             $0.uuid = .incrementing
             $0.continuousClock = clock
         }
-        
-        await store.send(.buttonTapped)
-        await store.receive(\.responsePictures) {
-            $0.pictureRows = rows
+        await store.send(.buttonTapped) {
             $0.started = true
-            $0.cascadeLoadingId = .init(0)
         }
-        
+        await store.receive(\.response) {
+            $0.rows = rows
+            $0.currentId = .init(0)
+        }
         await clock.run()
-        await store.receive(\.pictureRows[id: .init(0)].downloadComponent.download.updateProgress) {
-            $0.pictureRows[id: .init(0)]?.$downloadComponent.withLock { $0.status = .complete(.init()) }
+        await store.receive(\.rows[id: .init(0)].start) {
+            $0.rows[id: .init(0)]?.$count.withLock { $0 = 3 }
+            $0.rows[id: .init(1)]?.$count.withLock { $0 = 3 }
+            $0.rows[id: .init(2)]?.$count.withLock { $0 = 3 }
         }
-        await store.receive(\.pictureRows[id: .init(0)].downloadComponent.download.complete) {
-            $0.cascadeLoadingId = .init(1)
+        await store.receive(\.rows[id: .init(0)].increment)
+        await store.receive(\.rows[id: .init(0)].increment)
+        await store.receive(\.rows[id: .init(0)].increment)
+        await store.receive(\.rows[id: .init(0)].finish) {
+            $0.currentId = .init(1)
         }
+        await store.receive(\.rows[id: .init(1)].start)
+        await store.receive(\.rows[id: .init(1)].increment)
+        await store.receive(\.rows[id: .init(1)].increment)
+        await store.receive(\.rows[id: .init(1)].increment)
+        await store.receive(\.rows[id: .init(1)].finish) {
+            $0.currentId = .init(2)
+        }
+        await store.receive(\.rows[id: .init(2)].start)
+        await store.receive(\.rows[id: .init(2)].increment)
+        await store.receive(\.rows[id: .init(2)].increment)
+        await store.receive(\.rows[id: .init(2)].increment)
+        await store.receive(\.rows[id: .init(2)].finish)
     }
 }
